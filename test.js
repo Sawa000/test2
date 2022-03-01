9956cf6380592fed80ad9babdf0eb2225601f95d
Sawa000 <99480051+Sawa000@users.noreply.github.com>
2022-03-02 00:30:40 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,607 @@
+// ===================================メッセージプール==========================
+const textpuul =  [
+    {    // ID タイトル番号 タイトル 問題文==========
+        mondaibangou:0,
+        mondaiban: "0:",
+        taitoruna:"テストメッセージです",
+        monndaina: "問題テストテキストです",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDa",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 1,
+        botan_on_off1: 1,
+        botan_on_off2: 1,
+        botan_on_off3: 1,
+        botan_on_off4: 1,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDa0",
+        botanID1:"botanIDa1",
+        botanID2:"botanIDa2",
+        botanID3:"botanIDa3",
+        botanID4:"botanIDa4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDa",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:1,
+        mondaiban: "1",
+        taitoruna:"IDを入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDb",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDb0",
+        botanID1:"botanIDb1",
+        botanID2:"botanIDb2",
+        botanID3:"botanIDb3",
+        botanID4:"botanIDb4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDb",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:2,
+        mondaiban: "2",
+        taitoruna:"体温を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDc",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDc0",
+        botanID1:"botanIDc1",
+        botanID2:"botanIDc2",
+        botanID3:"botanIDc3",
+        botanID4:"botanIDc4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDc",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:3,
+        mondaiban: "3",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDb",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDb0",
+        botanID1:"botanIDb1",
+        botanID2:"botanIDb2",
+        botanID3:"botanIDb3",
+        botanID4:"botanIDb4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDb",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:4,
+        mondaiban: "4",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDe",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDe0",
+        botanID1:"botanIDe1",
+        botanID2:"botanIDe2",
+        botanID3:"botanIDe3",
+        botanID4:"botanIDe4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDe",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:5,
+        mondaiban: "5",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDf",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDf0",
+        botanID1:"botanIDf1",
+        botanID2:"botanIDf2",
+        botanID3:"botanIDf3",
+        botanID4:"botanIDf4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDf",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:6,
+        mondaiban: "6",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDg",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDg0",
+        botanID1:"botanIDg1",
+        botanID2:"botanIDg2",
+        botanID3:"botanIDg3",
+        botanID4:"botanIDg4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDg",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:7,
+        mondaiban: "7",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDh",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDh0",
+        botanID1:"botanIDh1",
+        botanID2:"botanIDh2",
+        botanID3:"botanIDh3",
+        botanID4:"botanIDh4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDh",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:8,
+        mondaiban: "8",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDh",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDh0",
+        botanID1:"botanIDh1",
+        botanID2:"botanIDh2",
+        botanID3:"botanIDh3",
+        botanID4:"botanIDh4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDh",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:9,
+        mondaiban: "9",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDi",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDi0",
+        botanID1:"botanIDi1",
+        botanID2:"botanIDi2",
+        botanID3:"botanIDi3",
+        botanID4:"botanIDi4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDi",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:10,
+        mondaiban: "10",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDi",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDi0",
+        botanID1:"botanIDi1",
+        botanID2:"botanIDi2",
+        botanID3:"botanIDi3",
+        botanID4:"botanIDi4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDi",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:11,
+        mondaiban: "11",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDi",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDi0",
+        botanID1:"botanIDi1",
+        botanID2:"botanIDi2",
+        botanID3:"botanIDi3",
+        botanID4:"botanIDi4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDi",
+    },{    // ID タイトル番号 タイトル 問題文==========
+        mondaibanngou:12,
+        mondaiban: "12",
+        taitoruna:"脈拍を入れてください",
+        monndaina: "",
+        // テキストbox_on_oof テキストboxID
+        textbox_on_off: 1,
+        textboxID: "textboxIDi",
+        // ボタン_on_off ボタン名前 ボタンID
+        botan_on_off0: 0,
+        botan_on_off1: 0,
+        botan_on_off2: 0,
+        botan_on_off3: 0,
+        botan_on_off4: 0,
+        botan0: "テスト0です",
+        botan1: "テスト1です",
+        botan2: "テスト2です",
+        botan3: "テスト3です",
+        botan4: "テスト4です",
+        botanID0:"botanIDi0",
+        botanID1:"botanIDi1",
+        botanID2:"botanIDi2",
+        botanID3:"botanIDi3",
+        botanID4:"botanIDi4",
+        // 次へ_no_off 
+        next_no_off: 1,
+        next:"次へ",
+        nextID:"nextIDi",
+    }
+]
+// ===================================定義域＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+let $perzi = 0;
+// 問題番号
+
+let $nannberID = document.getElementById("nannbar");
+$nannberID.textContent = textpuul[$perzi].mondaiban;
+// タイトル
+const $titleID = document.getElementById("title");
+$titleID.textContent = textpuul[$perzi].taitoruna;
+// 回答box
+let $textboxID = document.getElementById("textbox");
+let $input1 = document.createElement("input");
+$input1.setAttribute("type", "text");
+$input1.setAttribute("maxlength", "5");
+$input1.setAttribute("id", textpuul[$perzi].textboxID);
+$input1.setAttribute("size", "10");
+$textboxID.appendChild($input1);
+// 問題文
+const $mondaiID = document.getElementById("mondai");
+$mondaiID.textContent = textpuul[$perzi].monndaina;
+// 回答ボタン
+let botan0ID = document.getElementById("kaiyou0");
+let botan0ID1 = document.createElement("input");
+botan0ID1.setAttribute("type", "button");
+botan0ID1.setAttribute("id", textpuul[$perzi].botanID0);
+botan0ID1.setAttribute("value", textpuul[$perzi].botan0);
+botan0ID.appendChild(botan0ID1);
+
+let botan1ID = document.getElementById("kaiyou1");
+let botan1ID1 = document.createElement("input");
+botan1ID1.setAttribute("type", "button");
+botan1ID1.setAttribute("id", textpuul[$perzi].botanID1);
+botan1ID1.setAttribute("value", textpuul[$perzi].botan1);
+botan1ID.appendChild(botan1ID1);
+
+let botan2ID = document.getElementById("kaiyou2");
+let botan2ID1 = document.createElement("input");
+botan2ID1.setAttribute("type", "button");
+botan2ID1.setAttribute("id", textpuul[$perzi].botanID2);
+botan2ID1.setAttribute("value", textpuul[$perzi].botan2);
+botan2ID.appendChild(botan2ID1);
+
+let botan3ID = document.getElementById("kaiyou3");
+let botan3ID1 = document.createElement("input");
+botan3ID1.setAttribute("type", "button");
+botan3ID1.setAttribute("id", textpuul[$perzi].botanID3);
+botan3ID1.setAttribute("value", textpuul[$perzi].botan3);
+botan3ID.appendChild(botan3ID1);
+
+let botan4ID = document.getElementById("kaiyou4");
+let botan4ID1 = document.createElement("input");
+botan4ID1.setAttribute("type", "button");
+botan4ID1.setAttribute("id", textpuul[$perzi].botanID4);
+botan4ID1.setAttribute("value", textpuul[$perzi].botan4);
+botan4ID.appendChild(botan4ID1);
+//次へボタン
+let nexID = document.getElementById("sousinn");
+let nexID1 = document.createElement("input");
+nexID1.setAttribute("type", "button");
+nexID1.setAttribute("id", textpuul[$perzi].nextID);
+nexID1.setAttribute("value", textpuul[$perzi].next);
+nexID.appendChild(nexID1);
+// 確認ボタン
+let kak = document.getElementById("testbut");
+let kakID = document.createElement("input");
+kakID.setAttribute("type", "button");
+kakID.setAttribute("id", "aaaaa");
+kakID.setAttribute("value", "確認ボタン");
+kak.appendChild(kakID);
+
+// リセットボタン
+let rise = document.getElementById("sousinn");
+let riseID = document.createElement("input");
+riseID.setAttribute("type", "button");
+riseID.setAttribute("id", "bbbb");
+riseID.setAttribute("value", "リセット");
+rise.appendChild(riseID);
+
+const nextbotID = document.getElementById(textpuul[$perzi].nextID);
+const kakbotID = document.getElementById("aaaaa");
+const risebotID = document.getElementById("bbbb");
+// =============================実行=====================================
+
+nextbotID.addEventListener('click', () => {
+    // チェック域
+    if( $perzi === 0 ){
+        kakuninbutjs0 = document.getElementById("textboxIDa").value;
+    }
+    if( $perzi === 1 ){
+        kakuninbutjs1 = document.getElementById("textboxIDb").value;
+    }
+    if( $perzi === 2 ){
+        kakuninbutjs2 = document.getElementById("textboxIDc").value;
+    }
+
+    $perzi++;
+
+    $input1.setAttribute("id", textpuul[$perzi].textboxID);
+    botan0ID1.setAttribute("id", textpuul[$perzi].botanID0);
+    botan0ID1.setAttribute("value", textpuul[$perzi].botan0);
+    botan1ID1.setAttribute("id", textpuul[$perzi].botanID1);
+    botan1ID1.setAttribute("value", textpuul[$perzi].botan1);
+    botan2ID1.setAttribute("id", textpuul[$perzi].botanID2);
+    botan2ID1.setAttribute("value", textpuul[$perzi].botan2);
+    botan3ID1.setAttribute("id", textpuul[$perzi].botanID3);
+    botan3ID1.setAttribute("value", textpuul[$perzi].botan3);
+    botan4ID1.setAttribute("id", textpuul[$perzi].botanID4);
+    botan4ID1.setAttribute("value", textpuul[$perzi].botan4);
+    // 番号
+    $nannberID.textContent = textpuul[$perzi].mondaiban;
+    // タイトル
+    $titleID.textContent = textpuul[$perzi].taitoruna;
+    // 回答ボックス
+    let numbox = textpuul[$perzi].textbox_on_off;
+    if( numbox > 0 ) {
+        $textboxID.style.display = "block";
+    }else{
+        $textboxID.style.display = "none" ;
+    }
+    // 問題ボックス
+    $mondaiID.textContent = textpuul[$perzi].monndaina;
+
+    // ボタンBOX
+    let numbut0 = textpuul[$perzi].botan_on_off0;
+    let numbut1 = textpuul[$perzi].botan_on_off1;
+    let numbut2 = textpuul[$perzi].botan_on_off2;
+    let numbut3 = textpuul[$perzi].botan_on_off3;
+    let numbut4 = textpuul[$perzi].botan_on_off4;
+    if( numbut0 < 1 ) {
+        botan0ID.style.display="none";
+    }else{
+        botan0ID.style.display="block";
+    }
+    if( numbut1 < 1 ) {
+        botan1ID.style.display="none";
+    }else{
+        botan1ID.style.display="block";
+    }    
+    if( numbut2 < 1 ) {
+        botan2ID.style.display="none";
+    }else{
+        botan2ID.style.display="block";
+    }    
+    if( numbut3 < 1 ) {
+        botan3ID.style.display="none";
+    }else{
+        botan3ID.style.display="block";
+    }
+    if( numbut4 < 1 ) {
+        botan4ID.style.display="none";
+    }else{
+        botan4ID.style.display="block";
+    }
+
+    let numnex = textpuul[$perzi].next_no_off;
+    if( numnex < 1 ) {
+        nexID.style.display="none";
+    }else{
+        nexID.style.display="block";
+    }
+});
+
+
+kakbotID.addEventListener('click', () => {
+    const kakuninbut0 = document.getElementById("0");
+    kakuninbut0.innerText = kakuninbutjs0 ;
+    const kakuninbut1 = document.getElementById("1");
+    kakuninbut1.innerText = kakuninbutjs1;
+    const kakuninbut2 = document.getElementById("2");
+    kakuninbut2.innerText = kakuninbutjs2;
+});
+
+risebotID.addEventListener('click', () => {   
+    $perzi = 0;
+
+    $input1.setAttribute("id", textpuul[$perzi].textboxID);
+    botan0ID1.setAttribute("id", textpuul[$perzi].botanID0);
+    botan0ID1.setAttribute("value", textpuul[$perzi].botan0);
+    botan1ID1.setAttribute("id", textpuul[$perzi].botanID1);
+    botan1ID1.setAttribute("value", textpuul[$perzi].botan1);
+    botan2ID1.setAttribute("id", textpuul[$perzi].botanID2);
+    botan2ID1.setAttribute("value", textpuul[$perzi].botan2);
+    botan3ID1.setAttribute("id", textpuul[$perzi].botanID3);
+    botan3ID1.setAttribute("value", textpuul[$perzi].botan3);
+    botan4ID1.setAttribute("id", textpuul[$perzi].botanID4);
+    botan4ID1.setAttribute("value", textpuul[$perzi].botan4);
+    // 番号
+    $nannberID.textContent = textpuul[$perzi].mondaiban;
+    // タイトル
+    $titleID.textContent = textpuul[$perzi].taitoruna;
+    // 回答ボックス
+    let numbox = textpuul[$perzi].textbox_on_off;
+    if( numbox > 0 ) {
+        $textboxID.style.display = "block";
+    }else{
+        $textboxID.style.display = "none" ;
+    }
+    // 問題ボックス
+    $mondaiID.textContent = textpuul[$perzi].monndaina;
+
+    // ボタンBOX
+    let numbut0 = textpuul[$perzi].botan_on_off0;
+    let numbut1 = textpuul[$perzi].botan_on_off1;
+    let numbut2 = textpuul[$perzi].botan_on_off2;
+    let numbut3 = textpuul[$perzi].botan_on_off3;
+    let numbut4 = textpuul[$perzi].botan_on_off4;
+    if( numbut0 < 1 ) {
+        botan0ID.style.display="none";
+    }else{
+        botan0ID.style.display="block";
+    }
+    if( numbut1 < 1 ) {
+        botan1ID.style.display="none";
+    }else{
+        botan1ID.style.display="block";
+    }    
+    if( numbut2 < 1 ) {
+        botan2ID.style.display="none";
+    }else{
+        botan2ID.style.display="block";
+    }    
+    if( numbut3 < 1 ) {
+        botan3ID.style.display="none";
+    }else{
+        botan3ID.style.display="block";
+    }
+    if( numbut4 < 1 ) {
+        botan4ID.style.display="none";
+    }else{
+        botan4ID.style.display="block";
+    }
+
+    let numnex = textpuul[$perzi].next_no_off;
+    if( numnex < 1 ) {
+        nexID.style.display="none";
+    }else{
+        nexID.style.display="block";
+    }
+});
